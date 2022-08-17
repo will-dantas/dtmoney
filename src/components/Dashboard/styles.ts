dabc874b00b3bdd52b0da702f9a4e76625196743
william.dantas <william.dantas@WMD0822.sulamerica.com.br>
2022-08-17 11:17:37 -0300

    Changed width

--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -1,12 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  width: 1120px;
   margin: 0 auto;
   padding: 2.5rem 1rem;
 
   @media (max-width: 768px) {
-    width: 510px;
     margin: 0 1rem 2rem 1rem;
     padding: 2.5rem 1rem;
   }
